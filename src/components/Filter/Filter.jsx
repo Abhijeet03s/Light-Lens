1a623ac5c419ebfc355ffc9ac3c2ae47d94bdb1a
Abhijeet03s <abhijeetsharma476@gmail.com>
2022-11-18 03:05:52 +0530

    filter ui added

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -3,12 +3,47 @@ import React from "react";
 export default function Filter() {
   return (
     <>
-      <div className="max-w-full flex flex-col justify-start px-10">
-        <div className="w-full flex justify-between items-center">
-          <h1>Filter</h1>
-          <button>Clear</button>
+      <div className="font-Inter space-y-10">
+        <div className="flex justify-between items-center">
+          <h1 className="text-2xl">Filter</h1>
+          <button className="text-lg bg-gray-400 px-2 py-1 rounded-md">
+            Clear
+          </button>
         </div>
-        <div className="flex flex-col">ajflkas</div>
+        {/* Category */}
+        <div>
+          <select className="select w-full max-w-xs">
+            <option disabled selected>
+              Category
+            </option>
+            <option>Blueglasses</option>
+            <option>Sunglasses</option>
+            <option>Eyeglasses</option>
+          </select>
+        </div>
+        {/* Price */}
+        <div>
+          <select className="select w-full max-w-xs">
+            <option disabled selected>
+              Price
+            </option>
+            <option>Highest to Low</option>
+            <option>Low to Highest</option>
+          </select>
+        </div>
+        {/* Stars */}
+        <div>
+          <select className="select w-full max-w-xs">
+            <option disabled selected>
+              Rating
+            </option>
+            <option>1</option>
+            <option>2</option>
+            <option>3</option>
+            <option>4</option>
+            <option>5</option>
+          </select>
+        </div>
       </div>
     </>
   );
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -9,46 +9,50 @@ export default function Products() {
 
   return (
     <>
-      <div className="container max-w-full py-10 flex items-center">
-        {/* <Filter /> */}
-        <div className="max-w-full py-10 flex flex-col bg-[#f4f4f4] text-black space-y-10 font-Inter relative">
-          <h1 className="text-center text-5xl font-Mukta">Frames</h1>
-          <div className="flex flex-col items-center">
-            <div className="flex flex-wrap items-center gap-10">
-              {products.map((product) => {
-                return (
-                  <div
-                    key={product.id}
-                    className="flex flex-col justify-between w-80 overflow-hidden rounded-lg shadow-lg p-5 space-y-5 cursor-pointer"
-                  >
-                    <div>
-                      <Link to={`/products/${product.id}`}>
-                        <img src={product.image} alt="specs-image" />
-                      </Link>
-                    </div>
-                    <div className="flex flex-col justify-center items-start">
-                      <h1 className="text-xl font-bold">{product.title}</h1>
-                      <div className="flex items-center gap-x-1 mt-5">
-                        <span className="text-md font-medium">
-                          {product.rating}
-                        </span>
-                        <MdStar color="#4a99d3" />
+      <div className="container max-w-full py-10 flex flex-col items-center">
+        <div className="flex-1 flex">
+          <div className="basis-72 p-4">
+            <Filter />
+          </div>
+          <div className="flex-1 p-4">
+            <h1 className="text-center text-5xl font-Mukta">Frames</h1>
+            <div className="flex flex-col items-center">
+              <div className="flex flex-wrap justify-evenly items-center gap-10">
+                {products.map((product) => {
+                  return (
+                    <div
+                      key={product.id}
+                      className="flex flex-col justify-between w-80 overflow-hidden rounded-lg shadow-lg p-5 space-y-5 cursor-pointer"
+                    >
+                      <div>
+                        <Link to={`/products/${product.id}`}>
+                          <img src={product.image} alt="specs-image" />
+                        </Link>
                       </div>
-                      <div className="w-full flex justify-between items-center">
-                        <h2 className="text-lg mt-1 font-extrabold">
-                          ₹ {product.price}
-                        </h2>
-                        <div className="flex justify-between items-center bg-[#ed9526] text-white p-2 space-x-2 rounded-md">
-                          <img src="/cart.svg" alt="cart-icon" />
-                          <button onClick={() => handleAddToCart(product)}>
-                            Add To Cart
-                          </button>
+                      <div className="flex flex-col justify-center items-start">
+                        <h1 className="text-xl font-bold">{product.title}</h1>
+                        <div className="flex items-center gap-x-1 mt-5">
+                          <span className="text-md font-medium">
+                            {product.rating}
+                          </span>
+                          <MdStar color="#4a99d3" />
+                        </div>
+                        <div className="w-full flex justify-between items-center">
+                          <h2 className="text-lg mt-1 font-extrabold">
+                            ₹ {product.price}
+                          </h2>
+                          <div className="flex justify-between items-center bg-[#ed9526] text-white p-2 space-x-2 rounded-md">
+                            <img src="/cart.svg" alt="cart-icon" />
+                            <button onClick={() => handleAddToCart(product)}>
+                              Add To Cart
+                            </button>
+                          </div>
                         </div>
                       </div>
                     </div>
-                  </div>
-                );
-              })}
+                  );
+                })}
+              </div>
             </div>
           </div>
         </div>
