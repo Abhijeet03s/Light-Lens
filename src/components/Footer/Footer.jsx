c106ca3107f37ce1c4b168f3ca5c6e03ff790ed9
Abhijeet03s <abhijeetsharma476@gmail.com>
2024-10-15 16:59:18 +0530

    prod: fixed the production build

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,21 +1,21 @@
 import React from "react";
 import logoFooter from "../../assets/images/lightlens-footer.svg";
 import { AiOutlineGithub, AiOutlineTwitter, AiOutlineLinkedin } from "react-icons/ai";
+import { Link } from "react-router-dom";
 
 export default function Footer() {
   return (
     <>
       <footer className="bg-secondary font-Inter">
         <div className="container px-14 py-8 mx-auto flex items-center sm:flex-row flex-col">
-          <a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900"
-            href="/">
+          <Link to="/" className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
             <img
               className="h-6 w-auto"
               src={logoFooter}
               alt="LightLens"
               loading="lazy"
             />
-          </a>
+          </Link>
           <p className="text-sm text-white font-light sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">
             &copy; {new Date().getFullYear()} LightLens. All rights reserved.
           </p>
